531abb70666fb52fcc653dead19822473acb020d
ryotogashi <ryo.togashi.ca@gmail.com>
2020-02-22 17:06:08 -0800

    delete test

--- src/components/avatars/index.tsx
+++ src/components/avatars/index.tsx
@@ -1,14 +1,15 @@
-import * as React from "react";
+import * as React from 'react';
 
-import Section from "../app-components/Section";
-import IntroPage from "../app-components/IntroPage";
-import Avatar from "./Avatar";
-import avatar from "../../assets/sample-avatar.jpg";
+import Section from '../app-components/Section';
+import IntroPage from '../app-components/IntroPage';
+import avatar from '../../assets/sample-avatar.jpg';
+
+import { Avatar } from 'mise-en-place-ui';
 
 const Avatars = () => (
-  <Section title="Avatar">
-    <IntroPage title="Avatar" description="">
-      <Avatar src={avatar} height={"70px"} width={"70px"} />
+  <Section title='Avatar'>
+    <IntroPage title='Avatar' description=''>
+      <Avatar src={avatar} height={'70px'} width={'70px'} />
     </IntroPage>
   </Section>
 );
